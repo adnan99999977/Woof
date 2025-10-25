b2cd4f06370ef32e3f182ba6366e24e84b2695eb
Naif Adnan <146568103+adnan99999977@users.noreply.github.com>
2025-10-25 21:31:40 +0600

    some motion add

---
+++ src/components/useMobileGoogleRedirect.jsx
@@ -0,0 +1,29 @@
+import { getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+import { useEffect } from "react";
+import { auth } from "../Firebase/firebase.config"; // তোমার firebase config path
+
+const useMobileGoogleRedirect = (setUser, navigate, location) => {
+  useEffect(() => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      getRedirectResult(auth)
+        .then((result) => {
+          if (result) {
+            const currentUser = result.user;
+            setUser({
+              ...currentUser,
+              displayName: currentUser.displayName || "User Name",
+              photoURL: currentUser.photoURL || "/assets/profile.png",
+              email: currentUser.email,
+            });
+         
+            navigate(location.state?.pathname || "/");
+          }
+        })
+        .catch((error) => console.error("Google Redirect Error:", error));
+    }
+  }, [setUser, navigate, location]);
+};
+
+export default useMobileGoogleRedirect;
